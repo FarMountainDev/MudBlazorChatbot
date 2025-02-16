4a3880ede0efa7ea3f497e1054323224948cfc47
garrett <ghenricks@gmail.com>
2025-02-16 16:46:31 -0700

    Add project files.

---
+++ MudBlazorChatbot/Models/ChatBubble.cs
@@ -0,0 +1,9 @@
+﻿namespace MudBlazorChatbot.Models;
+
+public class ChatBubble
+{
+    public string? ThinkText { get; init; }
+    public string? ResponseText { get; init; }
+    public string? PromptText { get; init; }
+    public bool IsUser { get; init; }
+}
