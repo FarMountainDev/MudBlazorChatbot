3d2aca4b46def9b226671d67b42e75d50ad63d8e
garrett <ghenricks@gmail.com>
2025-02-18 11:49:00 -0700

    Add MudSnackbar notifications to download and delete operations, plus other minor UI and code improvements

--- MudBlazorChatbot/Models/ChatBubble.cs
+++ MudBlazorChatbot/Models/ChatBubble.cs
@@ -6,4 +6,6 @@ public class ChatBubble
     public string? ResponseText { get; init; }
     public string? PromptText { get; init; }
     public bool IsUser { get; init; }
+
+    public string GetClass() => IsUser ? "chat-bubble chat-bubble-user" : "chat-bubble chat-bubble-bot";
 }
