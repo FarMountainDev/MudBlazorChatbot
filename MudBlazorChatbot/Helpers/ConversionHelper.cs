52b2e92e358d6c1d801c80a2092c96a04cf53568
garrett <ghenricks@gmail.com>
2025-02-17 19:13:35 -0700

    Add popular models menu to download dialog and other UI and code improvements

---
+++ MudBlazorChatbot/Helpers/ConversionHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace MudBlazorChatbot.Helpers;
+
+public static class ConversionHelper
+{
+    public static string ConvertFileLengthToString(long fileLength)
+    {
+        string[] sizes = ["B", "KB", "MB", "GB", "TB"];
+        var order = 0;
+        var fileSize = (double)fileLength;
+        while (fileSize >= 1024 && order < sizes.Length - 1)
+        {
+            order++;
+            fileSize /= 1024;
+        }
+
+        return $"{fileSize:0.#} {sizes[order]}";
+    }
+}
